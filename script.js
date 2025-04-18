76fbbcab3c548b9c9b7111fc6223561216daccbb
instagram.com <sanjar3370062@gmail.com>
2025-04-18 14:14:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,156 @@
+var typed = new Typed('#element', {
+    strings: ['<i>Text Gradients</i>'],
+    typeSpeed: 40,
+    backSpeed: 30,           
+    backDelay: 1000,         
+    // loop: true,             
+    loopCount: Infinity,     
+    showCursor: true,        
+    cursorChar: '__',        
+    autoInsertCss: true,     
+    smartBackspace: true,   
+    shuffle: false,        
+    fadeOut: false,        
+    fadeOutClass: 'typed-fade-out', 
+    fadeOutDelay: 500,       
+    attr: null,              
+  });
+
+
+var typed = new Typed('#element1', {
+    strings: ['<i>Make your headlines stand out</i>'],  
+    typeSpeed: 30,
+    backSpeed: 10,           
+    backDelay: 1000,         
+    // loop: true,             
+    loopCount: Infinity,     
+    showCursor: true,        
+    cursorChar: '__',        
+    autoInsertCss: true,     
+    smartBackspace: true,   
+    shuffle: false,        
+    fadeOut: false,        
+    fadeOutClass: 'typed-fade-out', 
+    fadeOutDelay: 20,       
+    startDelay: 800,
+    attr: null,       
+});
+
+
+var typed = new Typed('#element2', {
+    strings: ['<i>Design is intelligence</i>'],
+    typeSpeed: 40,
+    backSpeed: 30,           
+    backDelay: 1000,         
+    // loop: true,             
+    loopCount: Infinity,     
+    showCursor: true,        
+    cursorChar: '__',        
+    autoInsertCss: true,     
+    smartBackspace: true,   
+    shuffle: false,        
+    fadeOut: false,        
+    fadeOutClass: 'typed-fade-out', 
+    fadeOutDelay: 500,       
+    attr: null,              
+})
+
+
+var typed = new Typed('#element3', {
+    strings: ['<i>made visible</i>'],
+    typeSpeed: 40,
+    backSpeed: 30,           
+    backDelay: 1000,         
+    // loop: true,             
+    loopCount: Infinity,     
+    showCursor: true,        
+    cursorChar: '__',        
+    autoInsertCss: true,     
+    smartBackspace: true,   
+    shuffle: false,        
+    fadeOut: false,        
+    fadeOutClass: 'typed-fade-out', 
+    fadeOutDelay: 500,   
+    startDelay: 1300,    
+    attr: null,              
+  });
+
+
+  var typed = new Typed('#element4', {
+    strings: ['<i>nteraction design isn’t merely a matter of aesthetic choice; rather,</i>'],
+    typeSpeed: 40,
+    backSpeed: 30,           
+    backDelay: 1000,         
+    // loop: true,             
+    loopCount: Infinity,     
+    showCursor: true,        
+    cursorChar: '__',        
+    autoInsertCss: true,     
+    smartBackspace: true,   
+    shuffle: false,        
+    fadeOut: false,        
+    fadeOutClass: 'typed-fade-out', 
+    fadeOutDelay: 500,       
+    attr: null,              
+  });
+
+
+
+  var typed = new Typed('#element5', {
+    strings: ['<i>it is based on an understanding of users and cognitive principles.</i>'],
+    typeSpeed: 40,
+    backSpeed: 30,           
+    backDelay: 1000,         
+    // loop: true,             
+    loopCount: Infinity,     
+    showCursor: true,        
+    cursorChar: '__',        
+    autoInsertCss: true,     
+    smartBackspace: true,   
+    shuffle: false,        
+    fadeOut: false,        
+    fadeOutClass: 'typed-fade-out', 
+    fadeOutDelay: 500,       
+    startDelay: 3900, 
+    attr: null,              
+  });
+
+
+  var typed = new Typed('#element6', {
+    strings: ['<i>Netflix</i>'],
+    typeSpeed: 40,
+    backSpeed: 30,           
+    backDelay: 1000,         
+    // loop: true,             
+    loopCount: Infinity,     
+    showCursor: true,        
+    cursorChar: '__',        
+    autoInsertCss: true,     
+    smartBackspace: true,   
+    shuffle: false,        
+    fadeOut: false,        
+    fadeOutClass: 'typed-fade-out', 
+    fadeOutDelay: 500,       
+    attr: null,              
+  });
+
+
+
+
+  var Typed = new Typed('#element7', {
+    strings: ['<i>Splash Screen</i>'],
+    typeSpeed: 40,
+    backSpeed: 30,           
+    backDelay: 1000,         
+    // loop: true,             
+    loopCount: Infinity,     
+    showCursor: true,        
+    cursorChar: '__',        
+    autoInsertCss: true,     
+    smartBackspace: true,   
+    shuffle: false,        
+    fadeOut: false,        
+    fadeOutClass: 'typed-fade-out', 
+    fadeOutDelay: 500,       
+    attr: null,            
+  });
